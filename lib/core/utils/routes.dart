30d8b073e2451564494412a2002d0f9cf4bf1ba1
Anas Tahir <138768398+anast439@users.noreply.github.com>
2026-03-18 14:36:38 +0500

    Create routes.dart

---
+++ lib/core/utils/routes.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:jooblie_app/core/utils/routes_name.dart';
+import 'package:jooblie_app/views/job_seeker/job_seeker_profile_view/jobseeker_profile_view.dart';
+import 'package:jooblie_app/views/login_screen.dart';
+import 'package:jooblie_app/views/main_dashboard_screen.dart';
+import 'package:jooblie_app/views/signup_screen.dart';
+import 'package:jooblie_app/views/splash_screen.dart';
+import 'package:jooblie_app/views/settings/settings_view.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.splash:
+        return MaterialPageRoute(builder: (_) =>  SplashScreen());
+      
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      
+      case RoutesName.signup:
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
+      
+      case RoutesName.dashboard:
+        final args = settings.arguments as Map<String, dynamic>?;
+        final isJobSeeker = args?['isJobSeeker'] ?? true;
+        return MaterialPageRoute(
+          builder: (_) => MainDashboardScreen(isJobSeeker: isJobSeeker),
+        );
+      
+      case RoutesName.settings:
+        return MaterialPageRoute(builder: (_) => const SettingsView());
+      case RoutesName.profileView:
+        return MaterialPageRoute(builder: (_) => const JobseekerProfileView());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
