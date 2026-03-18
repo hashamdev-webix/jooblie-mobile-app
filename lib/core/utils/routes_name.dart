1d2d5e1bdd9b1d7fb7f33cc73aaf3f8da262a336
Anas Tahir <138768398+anast439@users.noreply.github.com>
2026-03-18 14:36:49 +0500

    Create routes_name.dart

---
+++ lib/core/utils/routes_name.dart
@@ -0,0 +1,8 @@
+class RoutesName {
+  static const String splash = 'splash_screen';
+  static const String login = 'login_screen';
+  static const String signup = 'signup_screen';
+  static const String dashboard = 'main_dashboard_screen';
+  static const String settings = 'settings_view';
+  static const String profileView='profile_view';
+}
