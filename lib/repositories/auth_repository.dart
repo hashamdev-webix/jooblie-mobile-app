348bd12ae7c028441ed7d412d2f8b074ab4c7924
Anas Tahir <138768398+anast439@users.noreply.github.com>
2026-03-20 13:49:52 +0500

    Create auth_repository.dart

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,36 @@
+import 'package:jooblie_app/services/supabase_service.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthRepository {
+  final SupabaseService _supabaseService;
+
+  AuthRepository(this._supabaseService);
+
+  SupabaseClient get _client => _supabaseService.client;
+
+  Future<AuthResponse> signUp(String email, String password, Map<String, dynamic> data) async {
+    return await _client.auth.signUp(
+      email: email,
+      password: password,
+      data: data,
+    );
+
+
+
+  }
+
+  Future<AuthResponse> signIn(String email, String password) async {
+    return await _client.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _client.auth.signOut();
+  }
+
+  User? get currentUser => _client.auth.currentUser;
+  
+  Stream<AuthState> get authStateChanges => _client.auth.onAuthStateChange;
+}
