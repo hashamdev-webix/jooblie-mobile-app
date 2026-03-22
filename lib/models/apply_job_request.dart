858a9cec50748165bb6a18c1ae22746fbfc430ee
Anas Tahir <138768398+anast439@users.noreply.github.com>
2026-03-22 18:02:38 +0500

    Create apply_job_request.dart

---
+++ lib/models/apply_job_request.dart
@@ -0,0 +1,14 @@
+class ApplyJobRequest {
+  final String coverLetter;
+  final String? resumeUrl;
+
+  ApplyJobRequest({
+    required this.coverLetter,
+    this.resumeUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'cover_letter': coverLetter,
+    if (resumeUrl != null) 'resume_url': resumeUrl,
+  };
+}
